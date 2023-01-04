7e3a3488a2c7a9cf1915ba596cb545d99ed2be4a
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2023-01-04 21:49:43 +0530

    added files

---
+++ clsPyFiles/addition.py
@@ -0,0 +1,12 @@
+a = int(input("Enter num1: "))
+
+b = int(input("Enter num2: "))
+
+print(f"Addition is {a + b}")
+
+print(f"Difference is {a - b}")
+
+
+print(f"Multiplication is {a * b}")
+
+print(f"Division is {a / b}"
