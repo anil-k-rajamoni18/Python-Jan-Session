807d136840a535ebd2771f1e1fdb2244cb9b88c8
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2023-03-07 07:08:11 +0530

    added files

--- Flask_Tuts/flask-calci/application.py
+++ Flask_Tuts/flask-calci/application.py
@@ -1,5 +1,5 @@
 # importing 
-from flask import Flask ,render_template
+from flask import Flask ,render_template,request
 
 
 # create a object for Flask class
@@ -15,6 +15,36 @@ def index():
 def login():
   return render_template("login.html")
 
+@app.route("/greet/<name>")
+def greet(name):
+  return render_template("greet.html",username = name)
+
+
+@app.route("/calci")
+def calci():
+  return render_template("cal.html")
+
+@app.route("/calculation",methods=["GET","POST"])
+def calculation():
+  if request.method == "POST":
+    print(request.form["num1"],request.form["num2"],request.form["op"])
+    num1 = int(request.form["num1"])
+    num2 = int(request.form["num2"])
+    op = request.form["op"]
+
+    result = 0
+    if op=="+":
+      result = num1+num2
+    elif op=="-":
+     result = num1-num2
+    elif op=="*":
+     result = num1*num2
+    elif op=="/":
+     result = num1/num2
+    elif op=="%":
+     result = num1%num2
+     
+    return render_template("result.html", data=f"{num1} {op} {num2} = {result}")
 
 if __name__ == '__main__':
   app.run(debug=True,port=2211)
